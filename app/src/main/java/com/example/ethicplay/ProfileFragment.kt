ecd02e30927072634056fafeeab1fb860f3bbf0d
AbhijeetDubey1209 <abhijeetdubey48@gmail.com>
2024-09-20 03:57:45 +0530

    made changes in login page,added articles page,bottom nav bar and quiz page

---
+++ app/src/main/java/com/example/ethicplay/ProfileFragment.kt
@@ -0,0 +1,12 @@
+package com.example.ethicplay
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
+
