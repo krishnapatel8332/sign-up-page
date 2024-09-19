dfa0a1b24ce0bd124c43fc546c533c0f28daa435
Krishna Patel <kp8332064@gmail.com>
2024-09-20 04:00:38 +0530

    first update

--- app/src/main/java/com/example/ethicplay/ethicplay4.kt
+++ app/src/main/java/com/example/ethicplay/ethicplay4.kt
@@ -2,26 +2,24 @@ package com.example.ethicplay
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.postDelayed
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ethicplay4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_ethicplay4)
 
-       supportActionBar?.hide()
-        Handler().postDelayed({
-            val intent =Intent(this,ethicplay4::class.java)
+        val et4card=findViewById<CardView>(R.id.et4card)
+
+        et4card.setOnClickListener{
+            intent=Intent(applicationContext,ethicplay5::class.java)
             startActivity(intent)
-            finish()
-        },3000)
-
+        }
 
     }
 }
